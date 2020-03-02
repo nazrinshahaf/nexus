30e1f8537fccf3dcf89f7a6eef9e842a1a757e08
Nazrin Shah Adrian Fernandez <nazrin2222@gmail.com>
2020-03-02 19:10:03 +0800

    initial commit

--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,24 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import "./header.css"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <header>
+    <div className="navbar-contents">
+      <Link to="/" className="navbar-logo-container">
+        <div className="nexus-unique-logo-left"></div>
+        <div className="nexus-unique-logo-center"></div>
+        <div className="nexus-unique-logo-right"></div>
+        <div className="nexus-unique-logo-title">{siteTitle}</div>
+      </Link>
+
+      <div className="navbar-contents-container">
+        <div>About us</div>
+        <div>Our Services</div>
+        <div>Brands We Work With</div>
+        <div>Contact Us</div>
+      </div>
     </div>
   </header>
 )
