d698e8877239d3ab3160cc6d27de05856740f0a0
Nazrin Shah Adrian Fernandez <nazrin2222@gmail.com>
2020-03-03 17:50:21 +0800

    added content to about us page. Fixed header when in fixed position

--- src/components/layout.js
+++ src/components/layout.js
@@ -28,11 +28,11 @@ const Layout = ({ children }) => {
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
       <div
-      // style={{
-      //   margin: `0 auto`,
-      //   maxWidth: 960,
-      //   padding: `0 1.0875rem 1.45rem`,
-      // }}
+        style={{
+          margin: "110px 0 0 0",
+          //   maxWidth: 960,
+          // padding: `0 1.0875rem 1.45rem`,
+        }}
       >
         <main>{children}</main>
       </div>
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,49 @@
 import React from "react"
 import Layout from "../components/layout"
 
+import "../components/about.css"
+
 const About = () => (
   <Layout>
-    <h1>About Us</h1>
+    <div className="about-us-container">
+      <h1 className="about-us-title">About Us</h1>
+      <div className="about-us-header">
+        <p>
+          The establishment of Nexus Unique Sdn Bhd in 2005 was inspired by a
+          common goal by two partners who have worked extensively within the
+          industry and believe in delivering what the clients really want and
+          delivering results.
+        </p>
+      </div>
+    </div>
+    <div className="about-us-main-content">
+      <p>
+        This dedication infuses throughout our team and is visible in every
+        aspect of our business. We recognise the distinctive needs and
+        aspirations of different clients and work towards meeting these
+        different expectations. At Nexus Unique, we strive to deliver, not just
+        produce. Nexus Unique is made up of a two like-minded partners who
+        collectively possess more than 60 years of experience in the PR and
+        Marketing Communications profession. Our partners are professionals who
+        have worked for leading Malaysian conglomerates and understand from the
+        first-hand experience the demands and complexities of running a
+        successful business entity. This understanding allows us to architect
+        and execute communication plans that shape and transform public
+        perception without compromising business objectives. Nexus Unique is
+        further supported by a comprehensive network of talents from diverse
+        backgrounds, experts with proven capabilities in delivering success in
+        their specific discipline, industry or sector. This in turn allows us to
+        offer our services fine-tuned to the specific requirements of different
+        industries/businesses. Our team has extensive experience in the areas of
+        communications including branding, creative, multi-media, social media
+        as well as trainings. We have worked with some of the largest and most
+        respected organisations, international Public Relations firms, event
+        management companies, advertising agencies and other leading brands
+        prior to the formation of Nexus Unique. We also work with high-profile
+        individuals to help us achieve our clients’ strategic Public Relations
+        and marketing objectives across a broad range of interests.
+      </p>
+    </div>
   </Layout>
 )
 
