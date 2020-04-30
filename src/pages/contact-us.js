68778eed2116d11da17b461f715e03d3c4906d1c
Nazrin Shah Adrian Fernandez <nazrin2222@gmail.com>
2020-04-30 14:31:41 +0800

    one more test

--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -13,11 +13,12 @@ const Contact = () => (
       <div className="contact-line"></div>
     </div>
     <form
-      method="POST"
       name="nexus-contact-2"
+      method="post"
+      netlify-honeypot="bot-field"
       data-netlify="true"
-      data-netlify-honeypot="bot-field"
     >
+      <input type="hidden" name="bot-field" />
       <div className="contact-main-content">
         <div className="contact-input-row-container">
           <div className="contact-input-row-flex-box">
