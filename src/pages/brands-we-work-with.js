591212a5255179c364a955fba08cd93094c5d06f
Nazrin Shah Adrian Fernandez <nazrin2222@gmail.com>
2021-02-03 16:03:33 +0800

    changed image size of pc on brands page and change grid format for pc

--- src/pages/brands-we-work-with.js
+++ src/pages/brands-we-work-with.js
@@ -39,174 +39,235 @@ const BrandsWeWorkWith = () => (
       <h1 className="brands-header-title"> Brands We Work With</h1>
     </div>
     <div className="brands-main-content-alt">
-      <img
-        src={ARIES}
-        alt="Aries Telecoms"
-        className="ARIES"
-        width="230px"
-        height="180px"
-      ></img>
-      <img
-        src={ANTAH}
-        alt="Antah Schindler"
-        className="ANTAH"
-        width="350px"
-        height="160px"
-      ></img>
-      <img
-        src={ASIAIR}
-        alt="ASI Air Simpanan SDN BHD"
-        className="ASIAIR"
-        width="360px"
-        height="180px"
-      ></img>
-      <img
-        src={DGHAL}
-        alt="Dagang Halal"
-        className="DGHAL"
-        width="320px"
-        height="140px"
-      ></img>
-      <img
-        src={DIMDATA}
-        alt="Dimenstion Data"
-        className="DIMDATA"
-        width="360px"
-        height="240px"
-      ></img>
-      <img
-        src={GREENACRES}
-        alt="Green Acres"
-        className="GREENACRES"
-        width="350px"
-        height="150px"
-      ></img>
-      <img
-        src={HEITECH}
-        alt="Heitech"
-        className="HEITECH"
-        width="150px"
-        height="150px"
-      ></img>
-      <img
-        src={INVSELANG}
-        alt="Invest Selangor"
-        className="INVSELANG"
-        width="308px"
-        height="126px"
-      ></img>
-      <img
-        src={ISLIM}
-        alt="Islamic Liquidity Management"
-        className="ISLIM"
-        width="360px"
-      ></img>
-      <img
-        src={JARING}
-        alt="Jaring"
-        className="JARING"
-        width="360px"
-        height="150px"
-      ></img>
-      <img
-        src={MAMSB}
-        alt="MAMSB"
-        className="MAMSB"
-        height="180px"
-        width="160px"
-      ></img>
-      <img
-        src={MATRADE}
-        alt="MATRADE"
-        className="MATRADE"
-        width="250px"
-        height="125px"
-      ></img>
-      <img
-        src={MEDPRI}
-        alt="Media Prima"
-        className="MEDPRI"
-        width="240px"
-        height="140px"
-      ></img>
-      <img
-        src={MERTR}
-        alt="Merchantrade"
-        className="MERTR"
-        width="300px"
-        height="150px"
-      ></img>
-      <img src={MPOC} alt="MPOC" className="MPOC" width="200px"></img>
-      <img
-        src={MTDC}
-        alt="MTDC"
-        className="MTDC"
-        width="250px"
-        height="120px"
-      ></img>
-      <img
-        src={NEWPAR}
-        alt="New Paradigm"
-        className="NEWPAR"
-        width="400px"
-        height="110px"
-      ></img>
-      <img src={SAPURA} alt="Sapura" className="SAPURA" width="270px"></img>
-      <img
-        src={SHL}
-        alt="SHL"
-        className="SHL"
-        width="140px"
-        height="140px"
-      ></img>
-      <img
-        src={SPANCO}
-        alt="Spanco"
-        className="SPANCO"
-        width="400px"
-        height="110px"
-      ></img>
-      <img
-        src={UDAHB}
-        alt="UDA Holdings Berhad"
-        className="UDAHB"
-        height="120px"
-        width="280px"
-      ></img>
-      <img
-        src={UEM}
-        alt="UEM"
-        className="UEM"
-        width="205px"
-        height="115px"
-      ></img>
-      <img
-        src={UEMSUNRISE}
-        alt="UEM Sunrise"
-        className="UEMSUNRISE"
-        width="259px"
-      ></img>
-      <img src={UNI} alt="Union Investment" className="UNI" width="400px"></img>
-      <img
-        src={VTELE}
-        alt="V Telecoms Malaysia"
-        className="VTELE"
-        width="380px"
-      ></img>
-      <img
-        src={YYAF}
-        alt="Yayasan Albukhary Foundation"
-        className="YYAF"
-        width="180px"
-        height="180px"
-      ></img>
-      <img
-        src={YYP}
-        alt="Yayasan Peneraju Pendidikan Bumiputera"
-        className="YYP"
-        width="180px"
-        height="180px"
-      ></img>
+      <div>
+        <img
+          src={ARIES}
+          alt="Aries Telecoms"
+          className="ARIES"
+          width="138px"
+          height="108px"
+        ></img>
+      </div>
+      <div>
+        <img
+          src={ANTAH}
+          alt="Antah Schindler"
+          className="ANTAH"
+          width="245px"
+          height="112px"
+        ></img>
+      </div>
+      <div>
+        <img
+          src={ASIAIR}
+          alt="ASI Air Simpanan SDN BHD"
+          className="ASIAIR"
+          width="216px"
+          height="108px"
+        ></img>
+      </div>
+      <div>
+        <img
+          src={DGHAL}
+          alt="Dagang Halal"
+          className="DGHAL"
+          width="192px"
+          height="84px"
+        ></img>
+      </div>
+      <div>
+        <img
+          src={DIMDATA}
+          alt="Dimenstion Data"
+          className="DIMDATA"
+          width="216px"
+          height="144px"
+        ></img>
+      </div>
+      <div>
+        <img
+          src={GREENACRES}
+          alt="Green Acres"
+          className="GREENACRES"
+          width="189px"
+          height="81px"
+        ></img>
+      </div>
+      <div>
+        <img
+          src={HEITECH}
+          alt="Heitech"
+          className="HEITECH"
+          width="90px"
+          height="90px"
+        ></img>
+      </div>
+      <div>
+        <img
+          src={INVSELANG}
+          alt="Invest Selangor"
+          className="INVSELANG"
+          width="185px"
+          height="76px"
+        ></img>
+      </div>
+
+      <div>
+        <img
+          src={ISLIM}
+          alt="Islamic Liquidity Management"
+          className="ISLIM"
+          width="216px"
+        ></img>
+      </div>
+      <div>
+        <img
+          src={JARING}
+          alt="Jaring"
+          className="JARING"
+          width="216px"
+          height="90px"
+        ></img>
+      </div>
+
+      <div>
+        <img
+          src={MAMSB}
+          alt="MAMSB"
+          className="MAMSB"
+          height="108px"
+          width="96px"
+        ></img>
+      </div>
+      <div>
+        <img
+          src={MATRADE}
+          alt="MATRADE"
+          className="MATRADE"
+          width="150px"
+          height="75px"
+        ></img>
+      </div>
+      <div>
+        <img
+          src={MEDPRI}
+          alt="Media Prima"
+          className="MEDPRI"
+          width="144px"
+          height="84px"
+        ></img>
+      </div>
+      <div>
+        <img
+          src={MERTR}
+          alt="Merchantrade"
+          className="MERTR"
+          width="162px"
+          height="81px"
+        ></img>
+      </div>
+      <div>
+        <img src={MPOC} alt="MPOC" className="MPOC" width="120px"></img>
+      </div>
+      <div>
+        <img
+          src={MTDC}
+          alt="MTDC"
+          className="MTDC"
+          width="150px"
+          height="72px"
+        ></img>
+      </div>
+      <div>
+        <img
+          src={NEWPAR}
+          alt="New Paradigm"
+          className="NEWPAR"
+          width="240px"
+          height="66px"
+        ></img>
+      </div>
+      <div>
+        <img src={SAPURA} alt="Sapura" className="SAPURA" width="162px"></img>
+      </div>
+      <div>
+        <img
+          src={SHL}
+          alt="SHL"
+          className="SHL"
+          width="84px"
+          height="84px"
+        ></img>
+      </div>
+      <div>
+        <img
+          src={SPANCO}
+          alt="Spanco"
+          className="SPANCO"
+          width="240px"
+          height="66px"
+        ></img>
+      </div>
+      <div>
+        <img
+          src={UDAHB}
+          alt="UDA Holdings Berhad"
+          className="UDAHB"
+          height="72px"
+          width="168px"
+        ></img>
+      </div>
+      <div>
+        <img
+          src={UEM}
+          alt="UEM"
+          className="UEM"
+          width="123px"
+          height="69px"
+        ></img>
+      </div>
+      <div>
+        <img
+          src={UEMSUNRISE}
+          alt="UEM Sunrise"
+          className="UEMSUNRISE"
+          width="155px"
+        ></img>
+      </div>
+      <div>
+        <img
+          src={UNI}
+          alt="Union Investment"
+          className="UNI"
+          width="240px"
+        ></img>
+      </div>
+      <div>
+        <img
+          src={VTELE}
+          alt="V Telecoms Malaysia"
+          className="VTELE"
+          width="228px"
+        ></img>
+      </div>
+      <div>
+        <img
+          src={YYAF}
+          alt="Yayasan Albukhary Foundation"
+          className="YYAF"
+          width="108px"
+          height="108px"
+        ></img>
+      </div>
+      <div>
+        <img
+          src={YYP}
+          alt="Yayasan Peneraju Pendidikan Bumiputera"
+          className="YYP"
+          width="108px"
+          height="108px"
+        ></img>
+      </div>
     </div>
   </Layout>
 )
